9ccd4aa28dc4f3079e2a5085b76cc4c11602c35e
Tom Hanley <tjhanley.com@gmail.com>
2008-09-19 16:14:21 -0700

    added file header

--- jquery-breadcrumber.js
+++ jquery-breadcrumber.js
@@ -1,3 +1,15 @@
+/*  jquery-breadcrumber.js
+*  jquery-breadcrumber
+*  
+*  Created by Thomas Hanley on 2008-09-19.
+*  http://tjhanley.com
+*  Copyright 2008 thomas hanley
+*  Licensed under the MIT License: http://www.opensource.org/licenses/mit-license.php
+*/
+
+
+// ENJOY!
+
 (function($){
 	jQuery.fn.breadcrumber = function() {
 	  return this.each(function(i){
